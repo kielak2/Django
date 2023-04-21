1f3b91c3dece64a3b8f88515b60ddd247acd4801
Kielak2 <92437932+kielak2@users.noreply.github.com>
2023-04-21 10:52:29 +0200

    Add testing

---
+++ mysite/polls/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import Question, Choice
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
